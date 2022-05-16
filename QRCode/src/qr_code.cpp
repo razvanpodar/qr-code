85da40b630471762c8a3204536e679b4a24f53ac
Razvan Podar <razvan_podar@yahoo.com>
2022-05-16 10:04:21 +0300

    Add error correction level bits, mask bits and alignment patterns

--- QRCode/src/qr_code.cpp
+++ QRCode/src/qr_code.cpp
@@ -12,10 +12,6 @@ QRCode::~QRCode()
 
 void QRCode::GenerateCode()
 {
-	int version;
-	std::string v = std::string(m_version);	
-	std::istringstream(v) >> version;
-
 	unsigned char data[64] = {
 		100, 100, 100, 100, 100, 100, 100, 255,
 		100, 255, 255, 255, 255, 255, 100, 255,
@@ -28,6 +24,20 @@ void QRCode::GenerateCode()
 	};
 
 	m_locator = cv::Mat(8, 8, CV_8UC1, data);
+
+	unsigned char align_data[25] = {
+		100, 100, 100, 100, 100,
+		100, 255, 255, 255, 100,
+		100, 255, 100, 255, 100,
+		100, 255, 255, 255, 100,
+		100, 100, 100, 100, 100,
+	};
+
+	m_align = cv::Mat(5, 5, CV_8UC1, align_data);
+
+	int version;
+	std::string v = std::string(m_version);	
+	std::istringstream(v) >> version;
 
 	if (version > 0)
 		GenerateStandardCode(version);
@@ -46,6 +56,32 @@ void QRCode::GenerateMicroCode()
 	int micro_version = m_version[1] - 48;
 
 	m_size = M1_SIZE + ((micro_version - 1) * 2);
+	std::cout << micro_version << std::endl;
+
+	if ((micro_version == 1)
+		&& (!strcmp(m_error_correction, "M (15%)")
+			|| !strcmp(m_error_correction, "Q (25%)")
+			|| !strcmp(m_error_correction, "H (30%)")))
+	{
+		std::cout << "Error: wrong error correction level" << std::endl;
+		m_code.release();
+		return;
+	}	
+	if ((micro_version == 2 || micro_version == 3)
+		&& (!strcmp(m_error_correction, "Q (25%)")
+			|| !strcmp(m_error_correction, "H (30%)")))
+	{
+		std::cout << "Error: wrong error correction level" << std::endl;
+		m_code.release();
+		return;
+	}
+	if ((micro_version == 4) 
+		&& (!strcmp(m_error_correction, "H (30%)")))
+	{
+		std::cout << "Error: wrong error correction level" << std::endl;
+		m_code.release();
+		return;
+	}
 
 	// TODO: Set type based on the encoding
 	m_code = cv::Mat(m_size, m_size, CV_8UC1, 255);
@@ -65,13 +101,55 @@ void QRCode::GenerateMicroCode()
 	cv::Mat roi(m_code, cv::Rect(0, 0, 8, 8));
 	m_locator.copyTo(roi);
 
+	// Error correction level
+	unsigned short ecl = micro_ecl.at(MicroECL(micro_version, m_error_correction));
+	std::cout << "ECL: " << ecl << std::endl;
+	
+	int bit_1 = ecl & 1;
+	int bit_2 = (ecl >> 1) & 1;
+	int bit_3 = (ecl >> 2) & 1;
+
+	std::cout << bit_3 << bit_2 << bit_1 << std::endl;
+
+	if (bit_1)
+		m_code.at<uchar>(8, 3) = 0;
+	else
+		m_code.at<uchar>(8, 3) = 255;
+
+	if (bit_2)
+		m_code.at<uchar>(8, 2) = 0;
+	else
+		m_code.at<uchar>(8, 2) = 255;
+	
+	if (bit_3)
+		m_code.at<uchar>(8, 1) = 0;
+	else
+		m_code.at<uchar>(8, 1) = 255;
+
 	std::cout << "code: (" << m_code.rows << ","
 		<< m_code.cols << ")" << std::endl;
 	//std::cout << m_code << std::endl;
 
+	// Data mask
+	int data_mask = 1;
+	int data_mask_bit_1 = (data_mask) & 1;
+	int data_mask_bit_2 = (data_mask >> 1) & 1;
+
+	if (data_mask_bit_1)
+		m_code.at<uchar>(8, 5) = 0;
+	else
+		m_code.at<uchar>(8, 5) = 255;
+
+	if (data_mask_bit_2)
+		m_code.at<uchar>(8, 4) = 0;
+	else
+		m_code.at<uchar>(8, 4) = 255;
+
 	// Create buffer area
 	cv::copyMakeBorder(m_code, m_code, 2, 2, 2, 2, cv::BORDER_CONSTANT, 255);
 
+	// Save QR code
+	// Will be moved to a method and called at the press of a button
 	//cv::imwrite("test.png", m_code);
 }
 
@@ -94,7 +172,6 @@ void QRCode::GenerateStandardCode(int version)
 			else
 				m_code.at<uchar>(i, j) = 255;
 		}
-
 	}
 
 	// Place locators
@@ -111,10 +188,101 @@ void QRCode::GenerateStandardCode(int version)
 	cv::flip(m_locator, y_mirrored_locator, 0);
 	y_mirrored_locator.copyTo(roi_3);
 	
-	std::cout << "code: (" << m_code.rows << "," 
-		<< m_code.cols << ")" << std::endl;
-	//std::cout << m_code << std::endl;
+	// Place alignment patterns
+	if (version > 1)
+	{
+		std::vector<int> align_m = alignment_markers.at(version);
+		//std::cout << align_m << std::endl;
+		
+		for (int i = 0; i < align_m.size(); i++)
+		{
+			for (int j = 0; j < align_m.size(); j++)
+			{
+				if (align_m[i] < 8 && align_m[j] < 8)
+					continue;
+				if (align_m[i] < 8 && align_m[j] > (m_size - 8))
+					continue;
+				if (align_m[j] < 8 && align_m[i] > (m_size - 8))
+					continue;
+				cv::Mat roi_marker(m_code, cv::Rect(align_m[i] - 2, align_m[j] - 2, 5, 5));
+				std::cout << align_m[i] << ":" << align_m[j] << std::endl;
+
+				m_align.copyTo(roi_marker);
+				std::cout << "Done" << std::endl;
+
+			}
+		}
+	}
+
+	// Error correction level
+	int ecl = standard_ecl.at(m_error_correction);
+	
+	int bit_1 = ecl & 1;
+	int bit_2 = (ecl >> 1) & 1;
+
+	if (bit_1)
+		m_code.at<uchar>(8, 2) = 0;
+	else
+		m_code.at<uchar>(8, 2) = 255;
+
+	if (bit_2)
+		m_code.at<uchar>(8, 1) = 0;
+	else
+		m_code.at<uchar>(8, 1) = 255;
+
+	// Data mask
+	int data_mask = 1;
+	int data_mask_bit_1 = (data_mask) & 1;
+	int data_mask_bit_2 = (data_mask >> 1) & 1;
+	int data_mask_bit_3 = (data_mask >> 2) & 1;
+
+	if (data_mask_bit_1)
+		m_code.at<uchar>(8, 5) = 0;
+	else
+		m_code.at<uchar>(8, 5) = 255;
+
+	if (data_mask_bit_2)
+		m_code.at<uchar>(8, 4) = 0;
+	else
+		m_code.at<uchar>(8, 4) = 255;
+
+	if (data_mask_bit_3)
+		m_code.at<uchar>(8, 3) = 0;
+	else
+		m_code.at<uchar>(8, 3) = 255;
+
+
+	//std::cout << "code: (" << m_code.rows << "," 
+	//	<< m_code.cols << ")" << std::endl;
+	////std::cout << m_code << std::endl;
+
+
 
 	// Create buffer area
 	cv::copyMakeBorder(m_code, m_code, 4, 4, 4, 4, cv::BORDER_CONSTANT, 255);
 }
+
+bool QRCode::MicroMask(int i, int j, int mask)
+{
+	switch(mask) 
+	{
+	case 0:
+		if (i % 2 == 0)
+			return true;
+	case 1:
+		if (((i / 2) + (j / 3)) % 2 == 0)
+			return true;
+	case 2:
+		if ((i * j + (i * j) % 3) % 2 == 0)
+			return true;
+	case 3:
+		if ((i + j + (i * j) % 3) % 2 == 0)
+			return true;
+	default:
+		return false;
+	}
+
+	return false;
+}
+
+
--- QRCode/src/qr_code.h
+++ QRCode/src/qr_code.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <map>
+#include <vector>
 
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
@@ -12,16 +14,103 @@
 constexpr int M1_SIZE = 11;
 constexpr int V1_SIZE = 21;
 
+struct MicroECL 
+{
+	int version;
+	const char* ecl;
+
+	MicroECL(int v, const char* tecl)
+		: version(v), ecl(tecl)
+	{ }
+
+	bool operator< (const MicroECL& m_ecl) const
+	{
+		int d1 = version;
+		int d2 = m_ecl.version;
+
+		int i = 0;
+		while (ecl[i] != '\0')
+		{
+			d1 += ecl[i];
+			i++;
+		}
+
+		i = 0;
+		while (m_ecl.ecl[i] != '\0')
+		{
+			d2 += m_ecl.ecl[i];
+			i++;
+		}
+
+		return d1 < d2;
+	}
+};
+
+const std::map<MicroECL, unsigned short> micro_ecl = {
+	{ MicroECL(1, "L (7%)"), 0 },
+	{ MicroECL(2, "L (7%)"), 1 },
+	{ MicroECL(2, "M (15%)"), 2 },
+	{ MicroECL(3, "L (7%)"), 3 },
+	{ MicroECL(3, "M (15%)"), 4 },
+	{ MicroECL(4, "L (7%)"), 5 },
+	{ MicroECL(4, "M (15%)"), 6 },
+	{ MicroECL(4, "Q (25%)"), 7 }
+};
+
+const std::map<const char*, unsigned short> standard_ecl = {
+	{"L (7%)", 1},
+	{"M (15%)", 0},
+	{"Q (25%)", 3},
+	{"H (30%)", 2}
+};
+
+const std::map<int, std::vector<int>> alignment_markers = {
+{2, std::vector<int> { 6, 18 }}, {3, std::vector<int> { 6, 22 }},
+{4, std::vector<int> { 6, 26 }}, {5, std::vector<int> { 6, 30 }},
+{6, std::vector<int> { 6, 34 }}, {7, std::vector<int> { 6, 22, 38 }},
+{8, std::vector<int> { 6, 24, 42 }}, {9, std::vector<int> { 6, 26, 46 }},
+{10, std::vector<int> { 6, 28, 50 }}, {11, std::vector<int> { 6, 30, 54 }},
+{12, std::vector<int> { 6, 32, 58 }}, {13, std::vector<int> { 6, 34, 62 }},
+{14, std::vector<int> { 6, 26, 46, 66 }},
+{15, std::vector<int> { 6, 26, 48, 70 }},
+{16, std::vector<int> { 6, 26, 50, 74 }},
+{17, std::vector<int> { 6, 30, 54, 78 }},
+{18, std::vector<int> { 6, 30, 56, 82 }},
+{19, std::vector<int> { 6, 30, 58, 86 }},
+{20, std::vector<int> { 6, 34, 62, 90 }},
+{21, std::vector<int> { 6, 28, 50, 72, 94 }},
+{22, std::vector<int> { 6, 26, 50, 74, 98 }},
+{23, std::vector<int> { 6, 30, 54, 78, 102 }},
+{24, std::vector<int> { 6, 28, 54, 80, 106 }},
+{25, std::vector<int> { 6, 32, 58, 84, 110 }},
+{26, std::vector<int> { 6, 30, 58, 86, 114 }},
+{27, std::vector<int> { 6, 34, 62, 90, 118 }},
+{28, std::vector<int> { 6, 26, 50, 74, 98, 122 }},
+{29, std::vector<int> { 6, 30, 54, 78, 102, 126 }},
+{30, std::vector<int> { 6, 26, 52, 78, 104, 130 }},
+{31, std::vector<int> { 6, 30, 56, 82, 108, 134 }},
+{32, std::vector<int> { 6, 34, 60, 86, 112, 138 }},
+{33, std::vector<int> { 6, 30, 58, 86, 114, 142 }},
+{34, std::vector<int> { 6, 34, 62, 90, 118, 146 }},
+{35, std::vector<int> { 6, 30, 54, 78, 102, 126, 150 }},
+{36, std::vector<int> { 6, 24, 50, 76, 102, 128, 154 }},
+{37, std::vector<int> { 6, 28, 54, 80, 106, 132, 158 }},
+{38, std::vector<int> { 6, 32, 58, 84, 110, 136, 162 }},
+{39, std::vector<int> { 6, 26, 54, 82, 110, 138, 166 }},
+{40, std::vector<int> { 6, 30, 58, 86, 114, 142, 170 }}
+};
+
 class QRCode
 {
 private:
 	cv::Mat m_code;
 	cv::Mat m_locator;
+	cv::Mat m_align;
 	int m_size;
 
 public:
 	const char* m_version = "M1";
-	const char* m_error_correction = "L";
+	const char* m_error_correction = "L (7%)";
 	const char* m_encoding = "Alphanumeric";
 	char m_input[50] = { 0 };
 
@@ -39,4 +128,6 @@ public:
 private:
 	void GenerateMicroCode();
 	void GenerateStandardCode(int version);
+	bool MicroMask(int i, int j, int mask);
+	bool StandardMask(int mask);
 };
